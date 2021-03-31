6b3cbbc48b6cfd1b001d9f9426947ee033a67c9b
cgorrino <cgorrino03@gmail.com>
2021-03-30 23:26:48 -0700

    Adding function to switch the section that is visible on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var timerEl = document.querySelector("#timer-count");
 var startButton = document.querySelector("#start-button");
+var introSection = document.querySelector(".intro-container");
+var questionSection = document.querySelector(".question-container");
+var endSection = document.querySelector(".end-container");
+var scoresSection = document.querySelector(".scores-container");
 var count = 300; // start with 5 minutes
+
 function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function() {
@@ -16,6 +21,12 @@ function setTime() {
     }, 1000);
   }
 
+function switchSection(hideSection, showSection) {
+    hideSection.setAttribute("style", "display: none");
+    showSection.setAttribute("style", "display:block");
+}  
+
 startButton.addEventListener("click", function () {
     setTime();
+    switchSection(introSection, questionSection);
 })
