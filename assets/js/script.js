62183191e2cf16b5beb80b495e905ba57fca38a2
cgorrino <cgorrino03@gmail.com>
2021-03-31 00:18:14 -0700

    Beginning the function to change question text and answer options as the quiz goes on.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var introSection = document.querySelector(".intro-container");
 var questionSection = document.querySelector(".question-container");
 var endSection = document.querySelector(".end-container");
 var scoresSection = document.querySelector(".scores-container");
+var questionTextEl = document.querySelector("#question-text");
 var count = 300; // start with 5 minutes
 
 function setTime() {
@@ -24,7 +25,20 @@ function setTime() {
 function switchSection(hideSection, showSection) {
     hideSection.setAttribute("style", "display: none");
     showSection.setAttribute("style", "display:block");
-}  
+}
+
+function switchQuestion() {
+    var questionOne = {
+        questionText: "asdf",
+        answer1: "a",
+        answer2: "b",
+        answer3: "c",
+        answer4: "d",
+        correct: "a",
+    }
+    questionTextEl.textContent = questionOne.questionText;
+}
+switchQuestion();
 
 startButton.addEventListener("click", function () {
     setTime();
