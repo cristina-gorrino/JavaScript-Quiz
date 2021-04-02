cec9b561fa3a0a1250b441b92fcef3a7aa6f0362
cgorrino <cgorrino03@gmail.com>
2021-04-01 20:25:36 -0700

    WIP Adding function to check for correct answers and handle the next steps.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,8 @@ var answerText1El = document.querySelector("#answer1");
 var answerText2El = document.querySelector("#answer2");
 var answerText3El = document.querySelector("#answer3");
 var answerText4El = document.querySelector("#answer4");
+var resultRightEl = document.querySelector("#result-right");
+var resultWrongEl = document.querySelector("#result-wrong");
 
 var questionOne = {
     questionText: "asdf",
@@ -27,8 +29,10 @@ var questionTwo = {
     answer4: "h",
     correct: "e", 
 }
+var questionsArray = [questionOne, questionTwo];
+var i = 0; // index of the questions array, starts at question 1 and increments
 var count = 300; // start with 5 minutes
-var score = 0; // score is 5 points per correct question, then add remaining time (seconds)
+var score = 0; // score is 5 points per correct question, then add remaining time in seconds
 
 // This function controls the timer for the quiz. Starts when the start button is clicked
 function setTime() {
@@ -53,25 +57,43 @@ function switchSection(hideSection, showSection) {
 }
 
 // WIP- Updates the question text and set of responses from a set of objects
-function switchQuestion() {
-    var i = 0; // starts quiz at question 1
+function switchQuestion(i) {
+    //var i = 0; // starts quiz at question 1
 
-    // collect question objects in an array, then display them
-    var questionsArray = [questionOne, questionTwo];
-    function display(i) {
         questionTextEl.textContent = questionsArray[i].questionText;
         answerText1El.textContent = questionsArray[i].answer1;
         answerText2El.textContent = questionsArray[i].answer2;
         answerText3El.textContent = questionsArray[i].answer3;
         answerText4El.textContent = questionsArray[i].answer4;
-    }
-    display(i);
+
 
 
 }
 
-function checkIfCorrect() {
+function checkIfCorrect(element) {
     
+
+    var answeredCorrectly = false; // Tracks whether the current question is answered correctly, triggering the switch to the next question.
+    if (element.target.textContent === questionsArray[i].correct) {
+        console.log("the right answer!");
+        score += 5;
+        resultWrongEl.setAttribute("style", "display:none");
+        resultRightEl.setAttribute("style", "display:block");
+        answeredCorrectly = true;
+        i ++;
+        if (i > questionsArray.length-1){
+            console.log('end of the quiz');
+        } else {
+            switchQuestion(i);
+        }
+    } else {
+        resultRightEl.setAttribute("style", "display:none");
+        resultWrongEl.setAttribute("style", "display:block");
+        count -= 10;
+        timerEl.textContent = count;
+    }
+    console.log(element.target.textContent);
+    console.log(score);
 }
 
 
@@ -79,8 +101,9 @@ function checkIfCorrect() {
 startButton.addEventListener("click", function () {
     setTime();
     switchSection(introSection, questionSection);
-    switchQuestion();
+    switchQuestion(i);
 });
+
 
 // Event listeners for answer buttons on questions
 answerText1El.addEventListener("click", checkIfCorrect);
